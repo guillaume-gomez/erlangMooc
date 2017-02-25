363686562ccdabfa72b49d856934b5b8a13fb749
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-25 17:36:50 +0100

    continue workbook

--- recursive.erl
+++ recursive.erl
@@ -2,10 +2,16 @@
 -export([fib/1, pieces/1]).
 
 fib(0) -> 0;
-
 fib(1) -> 1;
-
 fib(N) -> fib(N - 1) + fib(N - 2).
 
 
+pieces(0) -> 1;
+pieces(1) -> 2;
+pieces(N) -> N + pieces(N-1).x
 
+
+multipieces(0,_) -> 1;
+multipieces(X,1) -> X+1;
+multipieces(X,N) ->
+  multipieces(X-1,N) + multipieces(X-1,N-1).

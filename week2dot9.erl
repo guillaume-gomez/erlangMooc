f55ad9b33f350ef70807af9797c16a210f837d8e
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-03-01 20:04:57 +0100

    add evens function

--- week2dot9.erl
+++ week2dot9.erl
@@ -1,5 +1,5 @@
 -module(week2dot9).
--export([double/1]).
+-export([double/1, evens/1]).
 
 % Transforming list elements
 % Define an Erlang function double/1 to double the elements of a list of numbers.
@@ -10,3 +10,16 @@ double([]) ->
 
 double([H | T]) ->
   [H * 2 | double(T)].
+
+
+%   Filtering lists
+% Define a function evens/1 that extracts the even numbers from a list of integers.
+
+evens([]) ->
+  [];
+
+evens([H | T]) when H rem 2 == 0 ->
+  [H |  evens(T) ];
+
+evens([_H| T]) ->
+  evens(T).

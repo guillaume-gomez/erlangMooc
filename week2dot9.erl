c5d10cec4514b15962dc21d169c53acff965598f
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-03-01 23:05:28 +0100

    wip' '

--- week2dot9.erl
+++ week2dot9.erl
@@ -1,5 +1,5 @@
 -module(week2dot9).
--export([double/1, evens/1, evensT/1]).
+-export([double/1, evens/1, evensT/1, median/1]).
 
 % Transforming list elements
 % Define an Erlang function double/1 to double the elements of a list of numbers.
@@ -40,3 +40,31 @@ evensT([H | T], List) when H rem 2 == 0 ->
 
 evensT([_H | T], List) ->
   evensT(T, List).
+
+% the median of a list of numbers: this is the middle element when the list is ordered (if the list is of even length you should average the middle two)
+myLength([]) ->
+	0;
+
+myLength([_H | T]) ->
+	1 + myLength(T).
+
+mySort(List) ->
+	mySort(List, 0, []).
+
+mySort([], )
+
+median(List) ->
+	median(List, 0, myLength(List)).
+
+median([First | _T ], Increment, Length) when Length rem 2 == 1 , Length div 2 == Increment  ->
+	First;
+
+
+median([First, Second | _T ], Increment, Length) when Length rem 2 == 0 , Length div 2 == (Increment + 1)  ->
+	(First + Second) / 2;
+
+median([_h | T], Increment, Length) ->
+	median(T, Increment + 1, Length).
+	
+% the modes of a list of numbers: this is a list consisting of the numbers that occur most frequently in the list; if there is is just one, this will be a list with one element only
+

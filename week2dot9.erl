cdd4b353f7c95fb17d091f20b465117c5315c1af
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-03-03 18:35:25 +0100

    wip sort

--- week2dot9.erl
+++ week2dot9.erl
@@ -1,5 +1,5 @@
 -module(week2dot9).
--export([double/1, evens/1, evensT/1, median/1, mergeSort/1]).
+-export([double/1, evens/1, evensT/1, median/1, myLength/1, mergeSort/1]).
 
 % Transforming list elements
 % Define an Erlang function double/1 to double the elements of a list of numbers.
@@ -48,22 +48,33 @@ myLength([]) ->
 myLength([_H | T]) ->
   1 + myLength(T).
 
-mergeSort(List) ->
-  mergeSort(List, , []).
+mergeSort([H| []]) ->
+  H;
+
+mergeSort(List) when is_list(List) ->
+  Middle = myLength(List) div 2,
+  Array1 = lists:sublist(List, Middle),
+  Array2 = lists:sublist(List, Middle+1, Middle+1),
+  merge(mergeSort(Array1), mergeSort(Array2)).
 
 merge([], List) ->
   List;
 
-merge(List, [], ) ->
+merge(List, []) ->
   List;
 
-merge([H1| T1], [H2| T2]) when H1 <= H2 ->
+merge(Val1, Val2) when Val1 =< Val2 ->
+  [Val1, Val2];
+
+merge(Val1, Val2) when Val1 > Val2 ->
+  [Val2, Val1];
+
+merge([H1| T1], [H2| T2]) when H1 =< H2 ->
   [H1, merge( T1 , [H2|T2])];
 
 
 merge([H1| T1], [H2| T2]) when H1 > H2 ->
-  [H2, merge( T2 , [H1|T1])];
-
+  [H2, merge( T2 , [H1|T1])].
 
 
 median(List) ->
@@ -72,12 +83,11 @@ median(List) ->
 median([First | _T ], Increment, Length) when Length rem 2 == 1 , Length div 2 == Increment  ->
   First;
 
-
 median([First, Second | _T ], Increment, Length) when Length rem 2 == 0 , Length div 2 == (Increment + 1)  ->
   (First + Second) / 2;
 
 median([_h | T], Increment, Length) ->
-	median(T, Increment + 1, Length).
+  median(T, Increment + 1, Length).
 
 % the modes of a list of numbers: this is a list consisting of the numbers that occur most frequently in the list; if there is is just one, this will be a list with one element only
 

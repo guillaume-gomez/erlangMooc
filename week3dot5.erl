651c69efad5178c2f50737c3659ae086615afc38
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-03-06 15:47:01 +0100

    add unzip function

--- week3dot5.erl
+++ week3dot5.erl
@@ -1,8 +1,9 @@
 -module(week3dot5).
--export([doubleAll/1, evens/1, product/1]).
+-export([doubleAll/1, evens/1, product/1, zip/2]).
 -spec doubleAll(list(number())) -> list(number()).
 -spec evens(list(integer())) -> list(integer()).
 -spec product(list(number())) -> number().
+-spec zip(list(number()), list(number())) -> list(tuple()).
 
 doubleAll(List) ->
   lists:map(fun double/1, List).
@@ -33,8 +34,17 @@ prod(X, Acc) ->
 % 24
 
 % a) Define a function zip/2 that “zips together” pairs of elements from two lists like this:
+zip(_ ,[]) ->
+  [];
 
-% zip([1,3,5,7], [2,4]) = [ {1,2}, {3,4} ]
+zip([], _) ->
+  [];
+
+zip([H1| T1], [ H2 | T2]) ->
+  [{H1, H2} | zip(T1,T2)].
+
+% week3dot5:zip([1,3,5,7], [2,4]).
+% [ {1,2}, {3,4} ]
 % where you can see that the elements from the longer list are lost.
 
 % b) Define a function zip_with/3 that “zips together” pairs of elements from two lists using the function in the first argument, like this:
